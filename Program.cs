46ddbb0007970b255ea732f9f1d33a2922b0cc30
Vini <vinicius.polimeno@aluno.senai.br>
2025-02-27 08:42:08 -0300

    feat: Realocando arquivos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using api_filmes_senai.Context;
+using api_filmes_senai.Interfaces;
+using api_filmes_senai.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona o contexto do banco de dados (exemplo com SQL Server)
+builder.Services.AddDbContext<Filmes_Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Adicionar o repositorio e a interface ao container da injeção de dependencia
+builder.Services.AddScoped<IGeneroRepository,GeneroRepository>();
+builder.Services.AddScoped<IFilmeRepository,FilmeRepository>();
+
+//Adicionar o serviço de controladores
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+//Adicionar o mapeamento dos controllers
+app.MapControllers();
+
+app.Run();
